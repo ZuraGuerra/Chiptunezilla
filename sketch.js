c4fe2336ef7ee8f81c2b6bfce87d08fc661ce56a
Victor Borja <vborja@apache.org>
2015-05-05 20:24:33 -0500

    Demonic triangles added

--- sketch.js
+++ sketch.js
@@ -3,24 +3,50 @@ function setup() {
   
 }
 
-var i = 1;
+
+var angelical_rotation = 0;
+var demonic_rotation = Math.PI;
+var rotation_step = Math.PI / 6;
+
+
+var angelical_color = 'rgba(0,0,255,0.25)';
+var demonic_color = 'rgba(255,0,0,0.25)';
+
+
+var last_sec = null;
 
 function draw() {
   clear();
-  
-  i += 1;
-
-  
   background(random(132, 255), random(132, 255), 0);
 
+
+  var secs = second();
+  console.log("current sec: ", secs);
+
+  if (last_sec !== secs) {
+
+    angelical_rotation += rotation_step;
+    demonic_rotation -= rotation_step;
+
+    last_sec = secs; 
+  }
+
+  noStroke();
+  fill(angelical_color);
+  generateTriangle(angelical_rotation);
+
+  fill(demonic_color);
+  generateTriangle(demonic_rotation);
+
+}
+
+function generateTriangle(rotation) {
   var zX = windowWidth/2;
   var zY = windowHeight/2;
 
   push();
-
   translate(zX, zY);
-  rotate (PI/random(0,6));
-
+  rotate(rotation);
 
   var side = 100;
   var j = sqrt((side*side) - (side/2 * side/2));
@@ -34,9 +60,8 @@ function draw() {
 
   triangle(aX, aY, bX, bY, cX, cY);
   pop();
-
 }
 
-//function windowResized() {
-  //resizeCanvas(windowWidth, windowHeight);
-//}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
